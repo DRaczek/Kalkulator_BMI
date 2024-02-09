09ae30f13303e83db8cf663c6027d024c591ae35
Damian Raczek <damian.raczek4206695@gmail.com>
2024-02-09 11:26:40 +0100

    Bmi calculation logic created.

--- Kalkulator_BMI/Kalkulator_BMI/MainPage.xaml.cs
+++ Kalkulator_BMI/Kalkulator_BMI/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -13,6 +14,24 @@ namespace Kalkulator_BMI
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //masa/wzrost^2
+            int weight = Convert.ToInt32(weightEntry.Text);
+            double height = Convert.ToInt32(heightEntry.Text)/100.0;
+            double bmi = weight / Math.Pow(height, 2);
+            string result = "";
+            if (bmi < 18.5) result = "Niedowaga";
+            else if (bmi >= 18.5 && bmi <= 24.99) result = "Waga prawidłowa";
+            else if (bmi >= 25 && bmi <= 29.99) result = "Nadwaga";
+            else if (bmi >= 30 && bmi <= 34.99) result = "Otyłość I stopnia";
+            else if (bmi >= 35 && bmi <= 39.99) result = "Otyłość II stopnia";
+            else if (bmi >= 40) result = "Otyłość III stopnia";
+
+            BmiLabel.Text = bmi.ToString();
+            ResultLabel.Text = result;
         }
     }
 }
