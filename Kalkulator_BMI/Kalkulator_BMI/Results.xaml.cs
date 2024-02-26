6efbe1de1fafd06088cc6e02869e27e302d2f24e
Damian Raczek <damian.raczek4206695@gmail.com>
2024-02-26 10:08:22 +0100

    Results page filled with saved results

--- Kalkulator_BMI/Kalkulator_BMI/Results.xaml.cs
+++ Kalkulator_BMI/Kalkulator_BMI/Results.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +11,37 @@ using Xamarin.Forms.Xaml;
 
 namespace Kalkulator_BMI
 {
+    public class ListViewItem
+    {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public BMI Bmi { get; set; }
+    }
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Results : ContentPage
     {
         public Results()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            string[] files =  Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            List<ListViewItem> list = new List<ListViewItem>();
+            foreach(string file in files) {
+                list.Add(
+                        new ListViewItem()
+                        {
+                            Bmi = JsonConvert.DeserializeObject<BMI>(File.ReadAllText(file)),
+                            Title =Path.GetFileName(file),
+                            Date = File.GetCreationTime(file)
+                        }    
+                        );
+            }
+            lstView.ItemsSource = list;
         }
     }
 }
